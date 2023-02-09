fcd1eade5b026c161132cde92aec8de039384d47
harryalaw <harryalaw@gmail.com>
2023-02-09 17:27:44 +0000

    Make spinner bigger

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,7 +2,7 @@
 export function Spinner() {
   return (
     <svg
-      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+      className="animate-spin -ml-1 mr-3 h-10 w-10 text-black"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
@@ -13,7 +13,7 @@ export function Spinner() {
         cy="12"
         r="10"
         stroke="currentColor"
-        stroke-width="4"
+        strokeWidth="4"
       ></circle>
       <path
         className="opacity-75"
